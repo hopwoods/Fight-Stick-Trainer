d60e025d6665fdc14543659934cb701248ac665a
Stuart Hopwood <stoo.hopwood@gmail.com>
2022-01-29 07:05:12 +0000

    update IEventStore.cs

--- ControllerInterface/Services/IEventStore.cs
+++ ControllerInterface/Services/IEventStore.cs
@@ -2,7 +2,7 @@ namespace ControllerInterface.Services;
 
 public interface IEventStore {
     
-    Dictionary<EventType, ComtrollerEvent> Events {get;, set;}
+    Dictionary<EventType, ComtrollerEvent> Events {get; set;}
     
     void RegisterEvents(Controller controller);
     
